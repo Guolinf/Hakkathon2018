7c248e70ef937cfc891b53ec9585d77ecb5076f3
Stefan Thorvardarson <stefanth@siminn.is>
2018-05-29 21:16:39 +0000

    polygons

--- googleTest.js
+++ googleTest.js
@@ -6,11 +6,24 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
     center: {lng: -21.926735, lat : 64.123924},
     scrollwheel: true,
-    zoom: 15,
+    zoom: 11,
     });
 
     var script = document.createElement('script');
+
+    map.data.setStyle(function(feature) {
+      var color = 'gray';
+      if (feature.getProperty('fill')) {
+        color = feature.getProperty('fill');
+      }
+      return /** @type {!google.maps.Data.StyleOptions} */({
+        fillColor: color,
+        fillOpacity: 0.2,
+        strokeWeight: 0
+      });
+    });
     
+
     google.maps.event.addListener(map, 'bounds_changed', function() {
         var bounds = map.getBounds()
         var upLeftlng = bounds.b.b
@@ -20,6 +33,8 @@ function initMap() {
         var str = 'http://157.157.9.120/litter?latmin='+ upLeftlat+'&latmax='+downRightlat+'&lonmin='+upLeftlng+'&lonmax='+ downRightlng;
         map.data.loadGeoJson(str);
     });
+    var str = 'http://157.157.9.120/bins'
+    map.data.loadGeoJson(str);
     
     google.maps.event.addListener(map, 'click', function(event) {
         placeMarker(event.latLng);
